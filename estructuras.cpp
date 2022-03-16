f026fa602ea7a8ce2a69394409c3653c62e70ca5
Marco Lanza <mlanzamolina@gmail.com>
2022-03-16 01:23:01 -0600

    vector cargar a variable

--- estructuras.cpp
+++ estructuras.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <string>
 #include <unistd.h>
-#include "nodo.h"
+#include <vector>
 using namespace std;
 
 void Disco::leerArchivogcc(){
@@ -24,31 +24,6 @@ void Disco::leerArchivogcc(){
     }
     string palabra="0";
     cout<<"*********Consulta Datos********\n\n";
-
-    cout<<"Ruta: "<<"\t\t"<<"Operacion: "<<"\n";
-
-  
-
-    while(getline(archivo,palabra,' ')){
-        nodo*prueba=0;
-        const char*b=palabra.c_str();
-        insertar(&prueba,b);
-       // cout<<palabra<<"\t\t";
-        sleep(2);
-    }
-
-    archivo.close();
-    imprimir(&prueba);
-}
-//function to mount gcc.trace
-void Disco::cargarArchivo(){
-    ifstream archivo("gcc.trace",ios::in);
-    if(!archivo){
-        cout<<"Error al abrir el archivo!"<<endl;
-        return;
-    }
-    string palabra="0";
-    cout<<"*********Carga Datos********\n\n";
 
     cout<<"Ruta: "<<"\t\t"<<"Operacion: "<<"\n";
 
@@ -79,4 +54,50 @@ void Disco::leerArchivobzip(){
     }
 
     archivo.close();
+}
+#include <chrono>
+
+/* Only needed for the sake of this example. */
+#include <iostream>
+#include <thread>
+
+void Disco::cargarArchivo()
+{
+     using std::chrono::high_resolution_clock;
+    using std::chrono::duration_cast;
+    using std::chrono::duration;
+    using std::chrono::milliseconds;
+
+    auto t1 = high_resolution_clock::now();
+   
+     //ifstream para leer archivos de texto
+    ifstream archivo("gcc.trace",ios::in);
+
+    if(!archivo){
+        cout<<"Error al abrir el archivo!"<<endl;
+        return;
+    }
+    string palabra="0";
+    cout<<"*********Cargando Datos********\n\n";
+
+    while(getline(archivo,palabra,'\n')){
+        //cout<<palabra<<"\t\t";
+        vector1.push_back(palabra);
+    }
+    archivo.close();
+
+      for (decltype(vector1.size()) i = 0; i <= vector1.size() - 1; i++)
+    {
+        cout << vector1[i] << endl;
+    }
+      auto t2 = high_resolution_clock::now();
+
+    /* Getting number of milliseconds as an integer. */
+    auto ms_int = duration_cast<milliseconds>(t2 - t1);
+
+    /* Getting number of milliseconds as a double. */
+    duration<double, std::milli> ms_double = t2 - t1;
+
+    std::cout << ms_double.count()/1000/60 << " minutos\n";
+
 }
--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <stdio.h>
 #include "estructuras.h"
-#include "nodo.h"
+
 using namespace std;
 
 int main(){
-    Disco d
-    d.leerArchivo();
-   
+    Disco d;
+    //d.leerArchivogcc();
+    d.cargarArchivo();
 }
