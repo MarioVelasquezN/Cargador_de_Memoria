8d3be1d012c9e0e07d6b389a41f8015f17ff9f2d
MarioVelasquezN <rolando.velasquez18@hotmail.com>
2022-03-17 14:16:02 -0600

    i ran

--- estructuras.cpp
+++ estructuras.cpp
@@ -63,7 +63,7 @@ void Disco::leerArchivobzip(){
 
 void Disco::cargarArchivo()
 {
-     using std::chrono::high_resolution_clock;
+    using std::chrono::high_resolution_clock;
     using std::chrono::duration_cast;
     using std::chrono::duration;
     using std::chrono::milliseconds;
